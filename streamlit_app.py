3204deaecce3334e72ca8c9f2f9063ba79b9862d
engfernandobatista-ui <eng.fernandobatista@gmail.com>
2025-12-07 17:00:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,62 @@ genre = st.radio(
     ["OK", "NOK", "N/A"],
     index=None,horizontal=True
 )
-
-
-
+genre = st.radio(
+    "A configuração dos equipamentos está conforme especificação do fabricante?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "A configuração dos equipamentos está conforme especificação do fabricante?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Foram detectados sinais de umidade, poeira ou sujeira no gabinete?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Existe sinal de corrosão e/ou ferrugem nos componentes do sistema?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Existe sinal de corrosão e/ou ferrugem nos componentes do sistema?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Há prensa-cabos frouxos, quebrados, não instalados, etc.?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "O gabinete do HSMS está selado e limpo?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Suporte e fixação em bom estado de conservação e adequados aos componentes?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Foi verificado se as fixações do gabinete estão no lugar e seguras?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+genre = st.radio(
+    "Foi verificado se as roscas estão lubrificadas no sensor SBSG?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+st.header("2. Considerações finais", divider="gray",text_alignment="center")
+genre = st.radio(
+    "Todas as anormalidades encontradas na inspeção foram sanadas?",
+    ["OK", "NOK", "N/A"],
+    index=None,horizontal=True
+)
+st.header("3. Comentários", divider="gray",text_alignment="center")
 
                   
