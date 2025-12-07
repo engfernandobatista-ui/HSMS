3e32df22eb07de0cc7b95c76b6d83956dea07414
engfernandobatista-ui <eng.fernandobatista@gmail.com>
2025-12-07 16:36:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,16 @@ import datetime
 st.title("Inspeção do HMS")
 
 title = st.text_input(" Embarcação", "")
-st.write("The current movie title is", title)
+title = st.text_input("Nota", "")
+title = st
+
+
+genre = st.radio(
+    "A identificação do painel e dos seus componentes estão visíveis e em bom estado de conservação (interno e externo)?",
+    ["OK", "NOK", "N/A"],
+    index=None,
+)
+
 
 d = st.date_input("Data de término da tarefa", datetime.date(2019, 7, 6))
 
