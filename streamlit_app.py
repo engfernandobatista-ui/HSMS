094f4da9b4082db5f5e09367ea22c6a25a8b3d88
engfernandobatista-ui <eng.fernandobatista@gmail.com>
2025-12-07 16:31:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import datetime
 
 st.title("Inspeção do HMS")
 
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+title = st.text_input(" Embarcação", "")
+st.write("The current movie title is", title)
 
-st.write ("Embarcação")
 d = st.date_input("Data de término da tarefa", datetime.date(2019, 7, 6))
 
                   
