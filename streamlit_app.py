3d68dec5873b2d4378318b10cbb15a5232fe1674
engfernandobatista-ui <eng.fernandobatista@gmail.com>
2025-12-07 16:25:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,12 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.title("Inspeção do HMS")
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+
+d = st.date_input("Embarcação", )
+d = st.date_input("Data de término da tarefa", datetime.date(2019, 7, 6))
+
+                  
