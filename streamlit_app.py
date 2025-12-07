1d7244a054cef523a6cf2c499c2e086b9a7721a9
engfernandobatista-ui <eng.fernandobatista@gmail.com>
2025-12-07 16:42:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("Inspeção do HMS")
 title = st.text_input(" Embarcação", "")
 title = st.text_input("Nota", "")
 
-st.header("Inspeção", divider="gray")
+st.header("Inspeção", divider="gray"), text_alignment="left"
 genre = st.radio(
     "A identificação do painel e dos seus componentes estão visíveis e em bom estado de conservação (interno e externo)?",
     ["OK", "NOK", "N/A"],
